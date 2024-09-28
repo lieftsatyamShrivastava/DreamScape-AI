9139453efa3486f6f9f6c2cd51396ff572d4b438
lieftsatyamsrivastava <satyamsrivastava384@gmail.com>
2024-09-28 17:50:22 +0530

    sales UI code

--- src/components/Sale.tsx
+++ src/components/Sale.tsx
@@ -4,10 +4,10 @@ import React from 'react'
 
 export const Sale = () => {
   return (
-    <div className="container mx-auto flex items-center justify-center bg-white text-black  relative z-50">
-      <span className="text-xl  font-bold ">
-      🔥 No credit card required. Start with our free tier today! 🔥
-      </span>
-    </div>
+    <div className="container mx-auto p-4 flex items-center justify-center bg-gradient-to-br from-slate-900 to-slate-700 text-white text-center sm:text-left relative z-50">
+  <span className="text-sm sm:text-xl font-bold">
+    🔥 No credit card required. Start with our free tier today! 🔥
+  </span>
+</div>
   )
 }
