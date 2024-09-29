7dc7acb6a5b06970007c522de7fdae27b23621c1
lieftsatyamsrivastava <satyamsrivastava384@gmail.com>
2024-09-29 22:25:47 +0530

    sales banner updates

--- src/components/Sale.tsx
+++ src/components/Sale.tsx
@@ -6,7 +6,7 @@ export const Sale = () => {
   return (
     <div className="container mx-auto p-4 flex items-center justify-center bg-gradient-to-br from-slate-900 to-slate-700 text-white text-center sm:text-left relative z-50">
   <span className="text-sm sm:text-xl font-bold">
-    🔥 No credit card required. Start with our free tier today! 🔥
+    🔥 Unlock Full Access with Our 30-Day Free Trial 🔥
   </span>
 </div>
   )
